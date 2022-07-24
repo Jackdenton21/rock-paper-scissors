c0f5eb44ba3f6c14173cd9dc95c66264347facc2
Jackdenton21 <Jackdenton21@gmail.com>
2022-07-24 19:03:39 -0400

    Working UI

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let buttons = document.querySelectorAll(".button");
+let cpuScore = 0;
+let playerScore = 0;
+
 function computerPlay(){
     let x = Math.floor(Math.random() * 3);
 
@@ -22,25 +26,37 @@ function playRound(playerSelection, computerSelection){
     }
 
     if (playerSelection == "rock" && computerSelection == "scissors"){
+        playerScore = ++playerScore
+        keepPlayerScore()
         return "You win! Rock beats Scissors"
     } 
     if (playerSelection == "rock" && computerSelection == "paper"){
+        cpuScore = ++cpuScore
+        keepCpuScore()
         return "You lose! Paper beats Rock"
     } 
 
 
     if (playerSelection == "paper" && computerSelection == "scissors"){
+        cpuScore = ++cpuScore
+        keepCpuScore()
         return "You lose! Scissors beat Paper"
     } 
     if (playerSelection == "paper" && computerSelection == "rock"){
+        playerScore = ++playerScore
+        keepPlayerScore()
         return "You win! Paper beats Rock"
     } 
 
 
     if (playerSelection == "scissors" && computerSelection == "paper"){
+        playerScore = ++playerScore
+        keepPlayerScore()
         return "You win! Scissors beat Paper"
     } 
     if (playerSelection == "scissors" && computerSelection == "rock"){
+        cpuScore = ++cpuScore
+        keepCpuScore()
         return "You lose! Rock beats Scissors"
     } 
 
@@ -69,3 +85,44 @@ function game(){
 
 }
 
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const img = button.querySelector("img");
+      playerSelection = img.alt.toLowerCase();
+     
+      playRound(playerSelection, computerPlay());
+  
+      //if (playerScore === 5 || computerScore === 5) {
+        //declareWinner();
+
+    });
+  });
+
+  function keepPlayerScore() {
+    let playerScoreBox = document.querySelector("#player-score");
+  
+    playerScoreBox.animate([{ opacity: 0 }, { opacity: 1 }], {
+      duration: 300,
+      fill: "forwards",
+      iterations: 1,
+      delay: 0,
+      easing: "ease-out",
+    });
+  
+    playerScoreBox.textContent = playerScore;
+  }
+
+
+  function keepCpuScore() {
+    let computerScoreBox = document.querySelector("#computer-score");
+  
+    computerScoreBox.animate([{ opacity: 0 }, { opacity: 1 }], {
+      duration: 300,
+      fill: "forwards",
+      iterations: 1,
+      delay: 0,
+      easing: "ease-out",
+    });
+  
+    computerScoreBox.textContent = cpuScore;
+  }
